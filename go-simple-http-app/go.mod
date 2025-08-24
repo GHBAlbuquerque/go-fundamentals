e8d5cfaaf05414e484a78324349f3016ce067883
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2025-08-24 15:12:08 -0300

    http: adding more HandleFunc for different pages

---
+++ go-simple-http-app/go.mod
@@ -0,0 +1,3 @@
+module go-simple-http-app
+
+go 1.25.0
--- go-simple-http-app/main.go
+++ go-simple-http-app/main.go
@@ -5,17 +5,30 @@ import (
 	"net/http"
 )
 
+const portNumber = ":8080"
+
+// Home is the home page handler
+func Home(writer http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(writer, "This is the home page")
+}
+
+// About is the about page handler
+func About(writer http.ResponseWriter, r *http.Request) {
+	sum := addValues(2, 2)
+	text := fmt.Sprintf("This is the about page. Sum is: %d", sum)
+	_, _ = fmt.Fprintf(writer, text)
+}
+
+// addValues adds two integers and returns the sum
+func addValues(x, y int) int {
+	return x + y
+}
+
 func main() {
 	//built in package of the standard library
-	http.HandleFunc("/", func(writer http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(writer, "Hello, world!")
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
 
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		fmt.Println("Bytes written:", n)
-	})
-
-	_ = http.ListenAndServe(":8080", nil)
+	fmt.Printf(fmt.Sprintf("Starting server on port %s...\n", portNumber))
+	_ = http.ListenAndServe(portNumber, nil)
 }
