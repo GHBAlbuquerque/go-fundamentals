13077bf83e47bf9117f48b90cbba5b5793385f00
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2025-08-24 15:22:50 -0300

    http: adds error handling

--- go-simple-http-app/main.go
+++ go-simple-http-app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
@@ -14,8 +15,21 @@ func Home(writer http.ResponseWriter, r *http.Request) {
 
 // About is the about page handler
 func About(writer http.ResponseWriter, r *http.Request) {
-	sum := addValues(2, 2)
-	text := fmt.Sprintf("This is the about page. Sum is: %d", sum)
+	result := addValues(2, 2)
+	text := fmt.Sprintf("This is the about page. Sum is: %d", result)
+	_, _ = fmt.Fprintf(writer, text)
+}
+
+// Divide is the about page handler
+func Divide(writer http.ResponseWriter, r *http.Request) {
+	result, err := divideValues(100, 0)
+
+	if err != nil {
+		fmt.Fprintf(writer, err.Error())
+		return
+	}
+
+	text := fmt.Sprintf("This is the divide page. Division is: %f", result)
 	_, _ = fmt.Fprintf(writer, text)
 }
 
@@ -24,10 +38,22 @@ func addValues(x, y int) int {
 	return x + y
 }
 
+// divideValues adds two integers and returns the sum
+func divideValues(x, y float32) (float32, error) {
+	if y <= 0 {
+		err := errors.New("Cannot divide by zero")
+		return 0, err
+	}
+
+	result := x / y
+	return result, nil
+}
+
 func main() {
 	//built in package of the standard library
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
+	http.HandleFunc("/divide", Divide)
 
 	fmt.Printf(fmt.Sprintf("Starting server on port %s...\n", portNumber))
 	_ = http.ListenAndServe(portNumber, nil)
